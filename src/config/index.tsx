97498ff9230c8b27c69690783d3f65731029407a
marbenz2 <benzinger.marcel@gmail.com>
2024-07-09 22:59:13 +0200

    fix test

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,3 +1,3 @@
 export const API_URL = import.meta.env.VITE_API_URL;
-export const INSTAGRAM_LIMIT = import.meta.env.VITE_INSTAGRAM_LIMIT ?? 12;
-export const INSTAGRAM_LL_TOKEN = import.meta.env.VITE_INSTAGRAM_LL_TOKEN;
+export const INSTAGRAM_LIMIT = process.env.VITE_INSTAGRAM_LIMIT ?? 12;
+export const INSTAGRAM_LL_TOKEN = process.env.VITE_INSTAGRAM_LL_TOKEN;
