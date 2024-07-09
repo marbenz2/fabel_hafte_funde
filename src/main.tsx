7568ccf75abb67b8c37f2efb6980acef93d230fe
marbenz2 <benzinger.marcel@gmail.com>
2024-07-09 22:12:57 +0200

    remove dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import Layout from "./Layout.tsx";
-import { ThemeProvider } from "./components/theme-provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <Layout>
-        <App />
-      </Layout>
-    </ThemeProvider>
+    <Layout>
+      <App />
+    </Layout>
   </React.StrictMode>
 );
