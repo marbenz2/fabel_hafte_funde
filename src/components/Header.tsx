868b35c135ea1234f195117f1fce8d4e25ee55d4
marbenz2 <benzinger.marcel@gmail.com>
2024-07-11 21:10:41 +0200

    style add layer for blur design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,21 +4,26 @@ import Openings from "./Openings";
 const Header = () => {
   return (
     <div className="relative flex h-screen w-full bg-header-pattern bg-cover lg:bg-contain bg-no-repeat bg-center lg:bg-right">
-      <div className="px-4 py-4 w-full bg-gradient-to-r from-white from-10% md:from-20% lg:from-40% xl:from-50% 2xl:from-60% to-transparent backdrop-saturate-0">
-        <div className="absolute top-1/4 transform -translate-y-1/4 -rotate-12 font-allura">
-          <h1 className="text-5xl sm:text-7xl lg:text-9xl xl:text-[12rem] 2xl:text-[15rem]">
-            Fabelhafte Funde
-          </h1>
-          <p className="text-xl sm:text-2xl lg:text-4xl">by Sandra Adamos</p>
+      <div className="p-0 md:p-14 w-full bg-gradient-to-r from-white from-10% md:from-20% lg:from-40% xl:from-50% 2xl:from-60% to-transparent">
+        <div className="relative flex h-full w-full shadow-none md:shadow-xl backdrop-blur-sm">
+          <div className="absolute left-4 top-1/4 transform -translate-y-1/4 -rotate-12 font-allura">
+            <h1 className="text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-[12rem] 2xl:text-[15rem]">
+              Fabelhafte Funde
+            </h1>
+            <p className="text-xl sm:text-2xl lg:text-4xl">by Sandra Adamos</p>
+          </div>
+
+          <div className="absolute bg-white/60 md:bg-transparent w-full md:w-fit p-4 bottom-1/4 transform translate-y-2/4 md:bottom-4 md:translate-y-0 left-0 md:left-4">
+            <Openings />
+          </div>
+          <ChevronsDown
+            className="absolute m-auto left-0 right-0 bottom-4 animate-bounce"
+            size="2.5rem"
+          >
+            &copy;
+          </ChevronsDown>
         </div>
-        <Openings />
       </div>
-      <ChevronsDown
-        className="absolute m-auto left-0 right-0 bottom-4 animate-bounce"
-        size="2.5rem"
-      >
-        &copy;
-      </ChevronsDown>
     </div>
   );
 };
