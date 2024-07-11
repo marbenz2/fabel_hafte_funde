668bc49df969003d927b04ebddf8f699b68d5fc7
marbenz2 <benzinger.marcel@gmail.com>
2024-07-11 21:09:49 +0200

    style moved styling div from component to parent component

--- src/components/Openings.tsx
+++ src/components/Openings.tsx
@@ -15,67 +15,65 @@ const today = new Intl.DateTimeFormat("de-DE", options)
 
 const Openings = () => {
   return (
-    <div className="absolute bg-white/60 md:bg-transparent w-full p-4 bottom-1/4 transform translate-y-2/4 md:bottom-0 md:translate-y-0 left-0">
-      <Table className="w-fit">
-        <TableHeader>
-          <TableRow>
-            <TableHead colSpan={8} className="text-lg">
-              Öffnungszeiten:
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow className={`${today === "MO" && "bg-black/30"}`}>
-            <TableCell className="text-left">MO</TableCell>
-            <TableCell>9:30</TableCell>
-            <TableCell className="w-fit p-0">-</TableCell>
-            <TableCell>13</TableCell>
-          </TableRow>
-          <TableRow className={`${today === "DI" && "bg-black/10"}`}>
-            <TableCell className="text-left">DI</TableCell>
-            <TableCell>9:30</TableCell>
-            <TableCell className="w-fit p-0">-</TableCell>
-            <TableCell>13</TableCell>
-          </TableRow>
-          <TableRow className={`${today === "MI" && "bg-black/10"}`}>
-            <TableCell className="text-left">MI</TableCell>
-            <TableCell>9:30</TableCell>
-            <TableCell className="w-fit p-0">-</TableCell>
-            <TableCell>13</TableCell>
-            <TableCell className="w-fit p-0">&</TableCell>
-            <TableCell>15</TableCell>
-            <TableCell className="w-fit p-0">-</TableCell>
-            <TableCell>18</TableCell>
-          </TableRow>
-          <TableRow className={`${today === "DO" && "bg-black/10"}`}>
-            <TableCell className="text-left">DO</TableCell>
-            <TableCell>9:30</TableCell>
-            <TableCell className="w-fit p-0">-</TableCell>
-            <TableCell>13</TableCell>
-            <TableCell className="w-fit p-0">&</TableCell>
-            <TableCell>15</TableCell>
-            <TableCell className="w-fit p-0">-</TableCell>
-            <TableCell>18</TableCell>
-          </TableRow>
-          <TableRow className={`${today === "FR" && "bg-black/10"}`}>
-            <TableCell className="text-left">FR</TableCell>
-            <TableCell>9:30</TableCell>
-            <TableCell className="w-fit p-0">-</TableCell>
-            <TableCell>13</TableCell>
-            <TableCell className="w-fit p-0">&</TableCell>
-            <TableCell>15</TableCell>
-            <TableCell className="w-fit p-0">-</TableCell>
-            <TableCell>18</TableCell>
-          </TableRow>
-          <TableRow className={`${today === "SA" && "bg-black/10"}`}>
-            <TableCell className="text-left">SA</TableCell>
-            <TableCell className="text-end">10</TableCell>
-            <TableCell className="w-fit p-0">-</TableCell>
-            <TableCell>14</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </div>
+    <Table className="w-fit">
+      <TableHeader>
+        <TableRow>
+          <TableHead colSpan={8} className="text-lg">
+            Öffnungszeiten:
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow className={`${today === "MO" && "bg-black/30"}`}>
+          <TableCell className="text-left">MO</TableCell>
+          <TableCell>9:30</TableCell>
+          <TableCell className="w-fit p-0">-</TableCell>
+          <TableCell>13</TableCell>
+        </TableRow>
+        <TableRow className={`${today === "DI" && "bg-black/10"}`}>
+          <TableCell className="text-left">DI</TableCell>
+          <TableCell>9:30</TableCell>
+          <TableCell className="w-fit p-0">-</TableCell>
+          <TableCell>13</TableCell>
+        </TableRow>
+        <TableRow className={`${today === "MI" && "bg-black/10"}`}>
+          <TableCell className="text-left">MI</TableCell>
+          <TableCell>9:30</TableCell>
+          <TableCell className="w-fit p-0">-</TableCell>
+          <TableCell>13</TableCell>
+          <TableCell className="w-fit p-0">&</TableCell>
+          <TableCell>15</TableCell>
+          <TableCell className="w-fit p-0">-</TableCell>
+          <TableCell>18</TableCell>
+        </TableRow>
+        <TableRow className={`${today === "DO" && "bg-black/10"}`}>
+          <TableCell className="text-left">DO</TableCell>
+          <TableCell>9:30</TableCell>
+          <TableCell className="w-fit p-0">-</TableCell>
+          <TableCell>13</TableCell>
+          <TableCell className="w-fit p-0">&</TableCell>
+          <TableCell>15</TableCell>
+          <TableCell className="w-fit p-0">-</TableCell>
+          <TableCell>18</TableCell>
+        </TableRow>
+        <TableRow className={`${today === "FR" && "bg-black/10"}`}>
+          <TableCell className="text-left">FR</TableCell>
+          <TableCell>9:30</TableCell>
+          <TableCell className="w-fit p-0">-</TableCell>
+          <TableCell>13</TableCell>
+          <TableCell className="w-fit p-0">&</TableCell>
+          <TableCell>15</TableCell>
+          <TableCell className="w-fit p-0">-</TableCell>
+          <TableCell>18</TableCell>
+        </TableRow>
+        <TableRow className={`${today === "SA" && "bg-black/10"}`}>
+          <TableCell className="text-left">SA</TableCell>
+          <TableCell className="text-end">10</TableCell>
+          <TableCell className="w-fit p-0">-</TableCell>
+          <TableCell>14</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
   );
 };
 
