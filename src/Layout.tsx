654344ef972f45c582814cf7dd2df0413f25931e
marbenz2 <benzinger.marcel@gmail.com>
2024-07-26 18:00:42 +0200

    style addBackgroundHouse item to Layout add Navigation component to Layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Header from "./components/Header";
 import BackgroundLeaves from "./components/background/BackgroundLeaves";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
+import BackgroundHouse from "./components/background/BackgroundHouse";
+import { Navigation } from "./components/Navigation";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="relative flex flex-col w-full min-h-screen h-full items-center bg-background-accent overflow-clip">
+    <main className="relative flex flex-col w-full max-w-[2560px] min-h-screen h-full items-center bg-background-accent overflow-clip">
+      <BackgroundHouse />
       <BackgroundLeaves />
+      <div className="absolute top-12 md:top-24 2xl:top-48 h-full w-full px-4 z-50">
+        <Navigation />
+      </div>
       <Header />
       {children}
       <Footer />
