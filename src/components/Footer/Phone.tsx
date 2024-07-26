68cc570f9c1c3a090fac5945cc2c19ff9a5fe540
marbenz2 <benzinger.marcel@gmail.com>
2024-07-26 20:22:01 +0200

    style text centered

--- src/components/Footer/Phone.tsx
+++ src/components/Footer/Phone.tsx
@@ -5,7 +5,10 @@ const Phone = () => {
   return (
     <div className="flex flex-col gap-2">
       <h3 className="text-xl font-semibold">Telefon</h3>
-      <a href={`tel:${address.phone.mobile}`} className="flex gap-2">
+      <a
+        href={`tel:${address.phone.mobile}`}
+        className="flex gap-2 items-center"
+      >
         <div className="flex gap-4">
           <PhoneIcon size="3em" />
         </div>
