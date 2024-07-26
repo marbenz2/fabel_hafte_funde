4fb935727d9ecee8465678ec90ccd201e05ef4b6
marbenz2 <benzinger.marcel@gmail.com>
2024-07-26 20:29:03 +0200

    style removed padding of header on mobile view

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,9 @@ const Header = () => {
   return (
     <section
       id="home"
-      className="flex min-h-screen w-full p-4 md:p-12 2xl:p-24 overflow-clip z-40"
+      className="flex min-h-screen w-full p-0 md:p-12 2xl:p-24 overflow-clip z-40"
     >
-      <div className="flex flex-col justify-between gap-16 w-full h-full p-8 md:p-12 2xl:p-24 bg-white/40 rounded-2xl backdrop-blur-sm overflow-clip">
+      <div className="flex flex-col justify-between gap-16 w-full h-full p-8 md:p-12 2xl:p-24 bg-white/40 rounded-b-2xl md:rounded-2xl backdrop-blur-sm overflow-clip">
         <Navigation />
         <Logo />
         <CallToAction />
