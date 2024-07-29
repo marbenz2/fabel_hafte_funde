0f630fb85e84b24ebf329a28a0e7152c11b77d38
marbenz2 <benzinger.marcel@gmail.com>
2024-07-29 17:53:58 +0200

    style changed backdrop filter for header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,9 +6,9 @@ const Header = () => {
   return (
     <section
       id="home"
-      className="flex min-h-screen w-full p-0 md:p-12 2xl:p-24 overflow-clip z-40"
+      className="flex min-h-screen w-full p-0 md:p-12 2xl:p-24 overflow-clip z-40 bg-white/0 backdrop-blur-[2px]"
     >
-      <div className="flex flex-col justify-between gap-16 w-full h-full p-8 md:p-12 2xl:p-24 bg-white/40 backdrop-blur-sm overflow-clip">
+      <div className="flex flex-col justify-between gap-16 w-full h-full p-8 md:p-12 2xl:p-24 overflow-clip">
         <Navigation />
         <Logo />
         <CallToAction />
