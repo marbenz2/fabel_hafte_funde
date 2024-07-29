791d651ba2fa653187ad0ce81b8582aa799445c1
marbenz2 <benzinger.marcel@gmail.com>
2024-07-29 17:19:48 +0200

    add store opening logic to show the current shop status

--- src/components/Openings.tsx
+++ src/components/Openings.tsx
@@ -2,6 +2,7 @@ import { StoreIcon } from "lucide-react";
 import { Card } from "./ui/card";
 import { Table, TableBody, TableCell, TableRow } from "./ui/table";
 import { Button } from "./ui/button";
+import React from "react";
 
 const options: Intl.DateTimeFormatOptions = { weekday: "long" };
 const today = new Intl.DateTimeFormat("de-DE", options)
@@ -9,80 +10,123 @@ const today = new Intl.DateTimeFormat("de-DE", options)
   .substring(0, 2)
   .toUpperCase();
 
+const openingHours: {
+  [key: string]: { name: string; hours: { open: string; close: string }[] };
+} = {
+  monday: {
+    name: "MO",
+    hours: [{ open: "09:30", close: "13:00" }],
+  },
+  tuesday: {
+    name: "DI",
+    hours: [{ open: "09:30", close: "13:00" }],
+  },
+  wednesday: {
+    name: "MI",
+    hours: [
+      { open: "09:30", close: "13:00" },
+      { open: "15:00", close: "18:00" },
+    ],
+  },
+  thursday: {
+    name: "DO",
+    hours: [
+      { open: "09:30", close: "13:00" },
+      { open: "15:00", close: "18:00" },
+    ],
+  },
+  friday: {
+    name: "FR",
+    hours: [
+      { open: "09:30", close: "13:00" },
+      { open: "15:00", close: "18:00" },
+    ],
+  },
+  saturday: {
+    name: "SA",
+    hours: [{ open: "10:00", close: "14:00" }],
+  },
+  sunday: {
+    name: "SO",
+    hours: [],
+  },
+};
+
+const isStoreOpen = () => {
+  const now = new Date();
+  const day = now.toLocaleString("en-US", { weekday: "long" }).toLowerCase();
+  const currentTime = now.getHours() * 60 + now.getMinutes();
+
+  const hours = openingHours[day]?.hours;
+  if (!hours || hours.length === 0) {
+    return false; // Store is closed all day
+  }
+
+  return hours.some(({ open, close }) => {
+    const [openHour, openMinute] = open.split(":").map(Number);
+    const [closeHour, closeMinute] = close.split(":").map(Number);
+
+    const openTime = openHour * 60 + openMinute;
+    const closeTime = closeHour * 60 + closeMinute;
+
+    return currentTime >= openTime && currentTime <= closeTime;
+  });
+};
+
+const StoreStatus = () => {
+  const storeOpen = isStoreOpen();
+  return (
+    <p
+      className={`text-sm ${
+        storeOpen ? "text-green-700" : "text-red-700"
+      } decoration-dashed underline`}
+    >
+      {storeOpen ? "Geöffnet" : "Geschlossen"}
+    </p>
+  );
+};
+
 const Openings = () => {
+  const days = Object.keys(openingHours);
+
   return (
     <div className="flex flex-col-reverse sm:flex-row justify-center items-center w-full gap-8">
       <div className="flex flex-col items-center gap-2">
         <Button variant="rounded" size="rounded">
           <StoreIcon stroke="white" />
         </Button>
-        <p className="text-sm text-green-700 decoration-dashed underline">
-          Jetzt geöffnet.
-        </p>
+        <StoreStatus />
       </div>
       <Card className="p-4 w-fit">
         <Table className="w-fit">
           <TableBody>
-            <TableRow className={`${today === "MO" && "bg-background-accent"}`}>
-              <TableCell className="text-left">MO</TableCell>
-              <TableCell>9:30</TableCell>
-              <TableCell className="w-fit p-0">-</TableCell>
-              <TableCell>13</TableCell>
-              <TableCell className="w-fit p-0"></TableCell>
-              <TableCell className="w-fit p-0"></TableCell>
-              <TableCell className="w-fit p-0"></TableCell>
-              <TableCell className="w-fit p-0"></TableCell>
-            </TableRow>
-            <TableRow className={`${today === "DI" && "bg-background-accent"}`}>
-              <TableCell className="text-left">DI</TableCell>
-              <TableCell>9:30</TableCell>
-              <TableCell className="w-fit p-0">-</TableCell>
-              <TableCell>13</TableCell>
-              <TableCell className="w-fit p-0"></TableCell>
-              <TableCell className="w-fit p-0"></TableCell>
-              <TableCell className="w-fit p-0"></TableCell>
-              <TableCell className="w-fit p-0"></TableCell>
-            </TableRow>
-            <TableRow className={`${today === "MI" && "bg-background-accent"}`}>
-              <TableCell className="text-left">MI</TableCell>
-              <TableCell>9:30</TableCell>
-              <TableCell className="w-fit p-0">-</TableCell>
-              <TableCell>13</TableCell>
-              <TableCell className="w-fit p-0">&</TableCell>
-              <TableCell>15</TableCell>
-              <TableCell className="w-fit p-0">-</TableCell>
-              <TableCell>18</TableCell>
-            </TableRow>
-            <TableRow className={`${today === "DO" && "bg-background-accent"}`}>
-              <TableCell className="text-left">DO</TableCell>
-              <TableCell>9:30</TableCell>
-              <TableCell className="w-fit p-0">-</TableCell>
-              <TableCell>13</TableCell>
-              <TableCell className="w-fit p-0">&</TableCell>
-              <TableCell>15</TableCell>
-              <TableCell className="w-fit p-0">-</TableCell>
-              <TableCell>18</TableCell>
-            </TableRow>
-            <TableRow className={`${today === "FR" && "bg-background-accent"}`}>
-              <TableCell className="text-left">FR</TableCell>
-              <TableCell>9:30</TableCell>
-              <TableCell className="w-fit p-0">-</TableCell>
-              <TableCell>13</TableCell>
-              <TableCell className="w-fit p-0">&</TableCell>
-              <TableCell>15</TableCell>
-              <TableCell className="w-fit p-0">-</TableCell>
-              <TableCell>18</TableCell>
-            </TableRow>
-            <TableRow className={`${today === "SA" && "bg-background-accent"}`}>
-              <TableCell className="text-left">SA</TableCell>
-              <TableCell className="text-end">10</TableCell>
-              <TableCell className="w-fit p-0">-</TableCell>
-              <TableCell>14</TableCell>
-              <TableCell className="w-fit p-0"></TableCell>
-              <TableCell className="w-fit p-0"></TableCell>
-              <TableCell className="w-fit p-0"></TableCell>
-              <TableCell className="w-fit p-0"></TableCell>
-            </TableRow>
+            {days
+              .filter((day) => day !== "sunday")
+              .map((day) => {
+                const { name, hours } = openingHours[day];
+                return (
+                  <TableRow
+                    key={day}
+                    className={`${today === name && "bg-background-accent"}`}
+                  >
+                    <TableCell className="text-left">{name}</TableCell>
+                    {hours.map(({ open, close }) => (
+                      <React.Fragment key={open}>
+                        <TableCell>{open}</TableCell>
+                        <TableCell className="w-fit p-0">-</TableCell>
+                        <TableCell>{close}</TableCell>
+                      </React.Fragment>
+                    ))}
+                    {hours.length === 1 && (
+                      <>
+                        <TableCell className="w-fit p-0"></TableCell>
+                        <TableCell className="w-fit p-0"></TableCell>
+                        <TableCell className="w-fit p-0"></TableCell>
+                      </>
+                    )}
+                  </TableRow>
+                );
+              })}
           </TableBody>
         </Table>
       </Card>
