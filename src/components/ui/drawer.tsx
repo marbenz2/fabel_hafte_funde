d9cf62aa5d9c09cf9f844b67c25109346ecef290
marbenz2 <benzinger.marcel@gmail.com>
2024-07-31 15:46:56 +0200

    style changed background color to bg-background

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -41,12 +41,12 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
+        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background rounded-none",
         className
       )}
       {...props}
     >
-      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-background-accent" />
+      <div className="mx-auto mt-4 h-2 w-[100px] rounded-full bg-background" />
       {children}
     </DrawerPrimitive.Content>
   </DrawerPortal>
