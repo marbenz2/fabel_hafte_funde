c8f6b7fad3082ec5a57036ac126320d2ea9a0801
marbenz2 <benzinger.marcel@gmail.com>
2024-07-31 18:17:13 +0200

    fix typo in classnames

--- src/components/info/Payment.tsx
+++ src/components/info/Payment.tsx
@@ -5,8 +5,8 @@ import { Heading2, Paragraph } from "../ui/text-formatter";
 const Payment = () => {
   return (
     <>
-      <Container id="payment" className="text-primary/60grid-cols-5">
-        <div className="order-2 col-span-5 lg:col-span-3 flex flex-col w-full h-full justify-center gap-8 py-32 px-4 lg:px-8 xl:px-12 2xl:px-32">
+      <Container id="payment" className="text-primary/60 grid-cols-5">
+        <div className="order-2 col-span-full lg:col-span-3 flex flex-col w-full h-full justify-center gap-8 py-32 px-4 lg:px-8 xl:px-12 2xl:px-32">
           <Heading2>Zahlungsarten</Heading2>
           <div className="flex gap-4 py-2 justify-center lg:justify-start">
             <div className="flex flex-col w-fit items-center">
@@ -30,7 +30,7 @@ const Payment = () => {
           </div>
         </div>
         <img
-          className="order-1 col-span-5 lg:col-span-2 max-h-[640px] h-full w-full object-cover"
+          className="order-1 col-span-full lg:col-span-2 max-h-[640px] h-full w-full object-cover"
           src="/payment.webp"
           alt="test"
         />
