2502f76d8c8a95a3fee445a11772b6f2af4750f0
marbenz2 <benzinger.marcel@gmail.com>
2024-07-31 15:48:18 +0200

    add text-formater components

--- src/components/info/Payment.tsx
+++ src/components/info/Payment.tsx
@@ -1,25 +1,25 @@
 import { CreditCardIcon, HandCoinsIcon, SmartphoneNfcIcon } from "lucide-react";
 import { Container } from "../ui/container";
-import { Heading2 } from "../ui/text-formatter";
+import { Heading2, Paragraph } from "../ui/text-formatter";
 
 const Payment = () => {
   return (
     <>
-      <Container className="text-primary/60 grid-cols-5">
+      <Container id="payment" className="text-primary/60 grid-cols-5">
         <div className="order-2 col-span-5 lg:col-span-3 flex flex-col w-full h-full justify-center gap-8 py-32 px-4 lg:px-8 xl:px-12 2xl:px-32">
           <Heading2>Zahlungsarten</Heading2>
           <div className="flex gap-4 py-2 justify-center lg:justify-start">
             <div className="flex flex-col w-fit items-center justify-center">
               <HandCoinsIcon size="3em" />
-              <p>Barzahlung</p>
+              <Paragraph>Barzahlung</Paragraph>
             </div>
             <div className="flex flex-col w-fit items-center justify-center">
               <CreditCardIcon size="3em" />
-              <p>Kartenzahlung</p>
+              <Paragraph>Kartenzahlung</Paragraph>
             </div>
             <div className="flex flex-col w-fit items-center justify-center">
               <SmartphoneNfcIcon size="3em" />
-              <p>Kontaktloses Bezahlen</p>
+              <Paragraph>Kontaktloses Bezahlen</Paragraph>
             </div>
           </div>
         </div>
