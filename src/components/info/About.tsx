5b4e0b890346e88ded34096e655920d98faab1b4
marbenz2 <benzinger.marcel@gmail.com>
2024-07-31 15:55:17 +0200

    add id to section for navigation

--- src/components/info/About.tsx
+++ src/components/info/About.tsx
@@ -4,7 +4,7 @@ import { Heading2, Paragraph } from "../ui/text-formatter";
 const About = () => {
   return (
     <>
-      <Container className="text-primary/60 grid-cols-5">
+      <Container id="about" className="text-primary/60 grid-cols-5">
         <div className="order-2 col-span-5 lg:col-span-3 flex flex-col w-full h-full justify-center gap-8 py-32 px-4 lg:px-8 xl:px-12 2xl:px-32">
           <Heading2>Über mich</Heading2>
           <Paragraph>
@@ -18,19 +18,12 @@ const About = () => {
             officia! Illo quia reiciendis quas aliquam animi modi impedit optio
             iste ipsa culpa? Architecto rem doloribus sequi tenetur illum nobis
             ea velit! Harum velit aliquam eligendi itaque sunt ullam, id
-            dolores! Sunt accusamus assumenda et ab! Illum aspernatur hic
-            expedita, eaque voluptas nulla quod consequuntur voluptatum quae
-            aliquam nemo sunt corporis natus perferendis magnam libero iure
-            distinctio explicabo corrupti, nam numquam? Voluptas doloremque
-            possimus id, adipisci, expedita minus repellendus quaerat
-            consectetur beatae perferendis, odit alias nihil repellat est? Enim
-            adipisci, voluptates voluptatibus iste ut aliquam qui corporis
-            dignissimos sunt, alias voluptatum.
+            dolores! Sunt accusamus assumenda et ab!
           </Paragraph>
         </div>
         <img
           className="order-1 col-span-5 lg:col-span-2 max-h-[640px] h-full w-full object-cover"
-          src="https://picsum.photos/1280/1280"
+          src="/about.webp"
           alt="test"
         />
       </Container>
