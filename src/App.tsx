7773dd52e380a0e2df18df57e4947d2fe5e5f17b
marbenz2 <benzinger.marcel@gmail.com>
2024-07-31 13:01:44 +0200

    add new components to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
+import Collection from "./components/collection/Collection";
 import About from "./components/info/About";
 import Commission from "./components/info/Commission";
 import Introduction from "./components/info/Introduction";
+import Payment from "./components/info/Payment";
+import Instagram from "./components/Instagram";
 import { Container } from "./components/ui/container";
 
 function App() {
   return (
     <>
       <div className="flex flex-col w-full">
-        <Container className="my-32">
+        <Container className="">
           <Introduction />
         </Container>
-        <Container className="">
+        <Container className="bg-black/10">
           <About />
         </Container>
-        <Container className="mb-32">
+        <Container className="">
           <Commission />
+        </Container>
+        <Container className="bg-black/10">
+          <Collection />
+        </Container>
+        <Container className="">
+          <Instagram />
+        </Container>
+        <Container className="bg-black/10 mb-32">
+          <Payment />
         </Container>
         {/*         <Container className="items-center py-12 px-4 md:px-12 2xl:px-24">
           <Collection />
