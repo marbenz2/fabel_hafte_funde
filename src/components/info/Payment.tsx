e425436f93cabed12509a13590748231b04b9e14
marbenz2 <benzinger.marcel@gmail.com>
2024-07-31 16:16:48 +0200

    style changed stroke color of icons to black style changed Paragraph classes for centering text properly

--- src/components/info/Payment.tsx
+++ src/components/info/Payment.tsx
@@ -5,21 +5,27 @@ import { Heading2, Paragraph } from "../ui/text-formatter";
 const Payment = () => {
   return (
     <>
-      <Container id="payment" className="text-primary/60 grid-cols-5">
+      <Container id="payment" className="text-primary/60grid-cols-5">
         <div className="order-2 col-span-5 lg:col-span-3 flex flex-col w-full h-full justify-center gap-8 py-32 px-4 lg:px-8 xl:px-12 2xl:px-32">
           <Heading2>Zahlungsarten</Heading2>
           <div className="flex gap-4 py-2 justify-center lg:justify-start">
-            <div className="flex flex-col w-fit items-center justify-center">
-              <HandCoinsIcon size="3em" />
-              <Paragraph>Barzahlung</Paragraph>
+            <div className="flex flex-col w-fit items-center">
+              <HandCoinsIcon stroke="black" size="3em" />
+              <Paragraph className="text-center leading-normal">
+                Barzahlung
+              </Paragraph>
             </div>
-            <div className="flex flex-col w-fit items-center justify-center">
-              <CreditCardIcon size="3em" />
-              <Paragraph>Kartenzahlung</Paragraph>
+            <div className="flex flex-col w-fit items-center">
+              <CreditCardIcon stroke="black" size="3em" />
+              <Paragraph className="text-center leading-normal">
+                Kartenzahlung
+              </Paragraph>
             </div>
-            <div className="flex flex-col w-fit items-center justify-center">
-              <SmartphoneNfcIcon size="3em" />
-              <Paragraph>Kontaktloses Bezahlen</Paragraph>
+            <div className="flex flex-col w-fit items-center">
+              <SmartphoneNfcIcon stroke="black" size="3em" />
+              <Paragraph className="text-center leading-normal">
+                Kontaktloses Bezahlen
+              </Paragraph>
             </div>
           </div>
         </div>
