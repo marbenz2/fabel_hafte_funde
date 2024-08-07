b3dfa477ea6d2a965ecce22a2938b5bfe98059f9
marbenz2 <benzinger.marcel@gmail.com>
2024-08-07 22:20:21 +0200

    feat: Add ContactButton to Commission component for scheduling appointments

--- src/components/info/Commission.tsx
+++ src/components/info/Commission.tsx
@@ -1,3 +1,4 @@
+import { ContactButton } from "../Contact";
 import { Container } from "../ui/container";
 import { Heading2, Paragraph } from "../ui/text-formatter";
 
@@ -6,27 +7,26 @@ const Commission = () => {
     <>
       <Container id="commission" className="text-primary/60 grid-cols-5">
         <div className="order-2 lg:order-1 col-span-5 lg:col-span-3 flex flex-col w-full h-full justify-center gap-8 py-32 px-4 lg:px-8 xl:px-12 2xl:px-32">
-          <Heading2>Annahme</Heading2>
+          <Heading2>Warenannahme</Heading2>
           <Paragraph>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            soluta ullam dolor atque praesentium beatae saepe non quaerat quasi.
-            Eius error quaerat natus, nam repudiandae voluptatem blanditiis
-            dicta quisquam? Quia. Voluptatibus repellendus eius nam, dicta,
-            perferendis cumque maxime enim aut quae quo voluptatum, eaque culpa
-            sit eum. Cupiditate deleniti sunt accusantium necessitatibus odio
-            commodi illo dolore, sint beatae qui vitae! Impedit distinctio eius
-            magni illum commodi deleniti nesciunt delectus quisquam quo earum in
-            repellendus laboriosam, non obcaecati placeat molestiae rerum,
-            dolores sint animi cumque quasi dolor esse blanditiis? Doloribus,
-            quia. Autem laborum reiciendis nihil distinctio eaque! Dolorum saepe
-            suscipit molestiae eius dignissimos corrupti totam, ullam porro
-            itaque eaque nobis iste beatae voluptas pariatur ut. Quia, eaque.
-            Vitae repellendus magnam dicta?
+            Bieten Sie Ihre Kleidung bequem und stressfrei in unserem Laden in
+            Tübingen zum Verkauf an. <br /> Stellen Sie sicher, dass Ihre
+            Kleidung gewaschen und gebügelt ist, bevor Sie sie zu uns bringen.
+            Wir übernehmen den Rest – von der ansprechenden Präsentation bis hin
+            zum Verkauf. Ihre Artikel erreichen in den nächsten 3-4 Monaten
+            viele potenzielle Käufer.
+            <br />
+            <br />
+            Vertrauen Sie auf unsere Erfahrung und lassen Sie uns gemeinsam
+            einen fairen Verkaufspreis erzielen!
+            <br />
+            Vereinbaren Sie jetzt einen Termin für die Anlieferung Ihrer Ware.
           </Paragraph>
+          <ContactButton />
         </div>
         <img
-          className="order-1 col-span-5 lg:col-span-2 max-h-[640px] h-full w-full object-cover"
-          src="/warenannahme.webp"
+          className="order-1 col-span-5 lg:col-span-2 h-full w-full object-cover"
+          src="/commission2.webp"
           alt="test"
         />
       </Container>
